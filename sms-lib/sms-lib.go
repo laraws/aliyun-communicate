6727fe307a8fa9e6606212857dc6607e1d5dcf2b
larrywang <40230381+laraws@users.noreply.github.com>
2025-04-30 17:21:17 +0800

    Update sms-lib.go

--- sms-lib/sms-lib.go
+++ sms-lib/sms-lib.go
@@ -42,7 +42,7 @@ type ALiYunCommunicationRequest struct {
 	OutId           string
 }
 
-var encoding = base32.NewEncoding("ybndrfg8ejkmcpqxot1uwisza345h697")
+var encoding = base32.StdEncoding
 
 func NewId() string {
 	var b bytes.Buffer
